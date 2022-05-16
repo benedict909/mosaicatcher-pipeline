c4c77cd42138c27f92c81992be13cebf0b8170ef
Monteiro <bmontei@sanders05.mdc-berlin.net>
2022-05-16 12:48:31 +0200

    change github repo link in utils/install_strandphaser.R so installation works

--- utils/install_strandphaser.R
+++ utils/install_strandphaser.R
@@ -1,4 +1,6 @@
 #!/usr/bin/env Rscript
 Sys.setenv(Renv='PWD')
 library(devtools)
-withr::with_libpaths(new = "utils/R-packages", install_git("git://github.com/daewoooo/StrandPhaseR.git", branch = "master"), "prefix")
+#withr::with_libpaths(new = "utils/R-packages", install_git("git://github.com/daewoooo/StrandPhaseR.git", branch = "master"), "prefix")
+# only works with this link
+withr::with_libpaths(new = "utils/R-packages", install_git("https://github.com/daewoooo/StrandPhaseR", ref = "master"), "prefix")
