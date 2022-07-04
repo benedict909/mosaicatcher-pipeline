c993af13336309338ab32b9e86b78473e31f8a6a
benedict909 <64800230+benedict909@users.noreply.github.com>
2022-07-04 16:42:35 +0200

    add bam file input correction

--- scTRIP_job.sh
+++ scTRIP_job.sh
@@ -17,6 +17,30 @@ echo $(pwd)
 # test working directory is set correctly
 [ ! $(ls | grep 'Snakefile' | wc -l) -ge 1 ] && { echo "ERROR: a Snakefile could not be found in $(pwd), make sure you are cd'ing into the cloned repo dir" ; exit ; }
 
+# correct input - scTRIP prefers "sort" not "sorted in the inout filenames
+for sample in $(ls bam)
+do
+        if [[ "$(ls bam/${sample}/selected)" == *sorted* ]] ; then
+
+                echo "correcting file names in $sample"
+                for mydir in all selected
+                do
+                        for myfile in $(ls bam/$sample/$mydir)
+                        do
+                                (
+                                        newname=$(echo $myfile | sed 's/sorted/sort/g')
+                                        mv bam/$sample/$mydir/$myfile bam/$sample/$mydir/$newname
+                                ) &
+                                if [[ $(jobs -r -p | wc -l) -ge $(nproc) ]]; # allows nproc number of jobs to be executed in parallel
+                                then
+                                        wait -n # if there are $(nproc) jobs running wait here for space to start next job
+                                fi
+                        done
+                        wait # wait for all jobs in the above loop to be done
+                done
+        fi
+done
+
 singularity exec --bind /fast docker://smei/mosaicatcher-pipeline-rpe1-chr3 snakemake \
     -j $(nproc) \
     --configfile Snake.config-singularity.BIH.json \
